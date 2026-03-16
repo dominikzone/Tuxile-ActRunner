2e7c1be4b9823cea5cdf46a7c7af3a5294bc0bb4
Jakub Kołaczyński <jakub.kolaczynski@proton.me>
2026-03-16 15:03:01 +0100

    chore: portable bash shebang and Python >=3.8 check

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
@@ -8,6 +8,12 @@ if ! command -v python3 &> /dev/null
 then
     echo "Python 3 is not installed. Please install Python 3.8+."
     exit 1
+fi
+
+if ! python3 -c 'import sys; exit(0 if sys.version_info >= (3,8) else 1)'
+then
+    echo "Python version is unsupported. Need Python 3.8+."
+    exit 2
 fi
 
 if ! command -v pip3 &> /dev/null
